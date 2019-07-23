9380ffc70f5d22781dea64892cf049f5be20ce41
vanessametonini <vanessametonini@gmail.com>
2019-07-23 19:14:21 -0300

    Validador assincrono com CEP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,101 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { map, catchError } from "rxjs/operators";
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+
+  formEndereco = new FormGroup({
+    cep: new FormControl('', [
+      Validators.required
+      ,Validators.minLength(8)
+      ,Validators.maxLength(8)
+      ,Validators.pattern('[0-9]{8}')
+    ],
+    this.validaCEP.bind(this)
+    )
+    ,logradouro: new FormControl({value: '', disabled: true})
+    ,complemento: new FormControl({value: '', disabled: true})
+    ,bairro: new FormControl({value: '', disabled: true})
+    ,localidade: new FormControl({value: '', disabled: true})
+    ,uf: new FormControl({value: '', disabled: true})
+  })
+
+  endereco = false;
+  erro = "";
+
+  constructor(private http: HttpClient) {}
+
+  validaCEP(control: AbstractControl){
+
+    return this
+            .http
+            .get(`https://viacep.com.br/ws/${control.value}/json/`)
+            .pipe(
+              map((response: any )=>{
+                if(response.erro) {
+                  return {cepInvalido: response.erro}
+                }
+                return null
+              })
+              ,catchError(
+                (response: HttpErrorResponse) => [{cepInvalido: !response.ok}]
+              )
+            )
+
+  }
+
+  consultar(){
+
+    const cep = this.formEndereco.get('cep').value;
+
+    if(this.formEndereco.invalid){
+      this.formEndereco.markAllAsTouched();
+      return
+    }
+
+    this.http
+        .get(`https://viacep.com.br/ws/${cep}/json/`)
+        .subscribe(
+          (dados: Endereco) => {
+
+            //console.log(dados);
+
+            if(dados.erro) {
+              this.erro = `CEP ${cep} não encontrado!`;
+              return
+            }
+
+            if(this.erro) this.erro = "";
+
+            this.formEndereco.patchValue(dados);
+            this.endereco = true;
+          },
+          erro => {
+            //console.error(erro)
+            this.formEndereco.reset();
+            this.erro = "Erro no CEP, o mesmo deve conter apenas 8 números"
+          }
+        )
+  }
+}
+
+class Endereco {
+
+  cep = "";
+  logradouro = "";
+  complemento = "";
+  bairro = "";
+  localidade = "";
+  uf = "";
+  unidade = "";
+  ibge = "";
+  gia = "";
+  erro = "";
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
